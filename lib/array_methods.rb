6404a6bf571ace58ea55bdfa6a0345bc0fef2164
sasha walker <sashawalker31@gmail.com>
2019-08-01 01:42:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,8 @@
 def find_element_index(array, value_to_find)
   scale=(["2","3","0","5"])
-  p scale.index(scale,2)
+  p scale.each_with_index do |scale, index|
+    p "the index is #{index}"
+    p "the value is #{scale}"
 end
 
 def find_max_value(array)
