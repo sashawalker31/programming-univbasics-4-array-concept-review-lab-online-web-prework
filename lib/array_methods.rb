2c4bd57137b1c81a9fd44c0f98edc7b024396ef5
sasha walker <sashawalker31@gmail.com>
2019-08-01 01:09:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  scale=(["2","3","4","5"])
+  scale.index("0")
 end
 
 def find_max_value(array)
-  # Add your solution here
+  scale=(["2","3","4","5"])
+  scale.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  scale=(["2","3","4","5"])
+  scale.min
 end
